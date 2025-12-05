72416991a2bcf11780f1be8569153d13eb0c60ce
dilsec20 <dilipkumar2832005@gmail.com>
2025-12-06 01:19:59 +0530

    fix(problems): cleaned Binary Search duplicated sample description

---
+++ server/fix_overwrite.sql
@@ -0,0 +1,16 @@
+UPDATE problems 
+SET description = 'Given the root of a binary tree, determine if it is a valid binary search tree (BST).
+
+A valid BST is defined as follows:
+- The left subtree of a node contains only nodes with keys less than the node''s key.
+- The right subtree of a node contains only nodes with keys greater than the node''s key.
+- Both the left and right subtrees must also be binary search trees.
+
+**Constraints:**
+- The number of nodes in the tree is in the range [1, 10^4].
+- -2^31 <= Node.val <= 2^31 - 1
+
+**Source:** LeetCode #98',
+test_case_input = '2 1 3',
+test_case_output = 'true'
+WHERE title = 'Validate Binary Search Tree';

81a7338f4c06b565fdc090430374f4f63eb49290
dilsec20 <dilipkumar2832005@gmail.com>
2025-12-06 00:09:24 +0530

    fix(dsa): removed all videos from Time & Space Complexity module

---
+++ server/fix_intro.sql
@@ -0,0 +1,1 @@
+UPDATE dsa_topics SET video_url = NULL WHERE title ILIKE '%Introduction to Program%';
---
+++ server/fix_time.sql
@@ -0,0 +1,1 @@
+UPDATE dsa_topics SET video_url = NULL WHERE title ILIKE '%Time & Space Complexity%';

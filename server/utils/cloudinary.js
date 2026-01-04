d4dd0b1b6420441d4c2ea96ecb0e126a0a2938f3
dilsec20 <dilipkumar2832005@gmail.com>
2026-01-05 02:18:45 +0530

    fix(security): move cloudinary credentials to .env

--- server/utils/cloudinary.js
+++ server/utils/cloudinary.js
@@ -3,9 +3,9 @@ const { CloudinaryStorage } = require('multer-storage-cloudinary');
 const multer = require('multer');
 
 cloudinary.config({
-    cloud_name: 'dcxmwr4mj',
-    api_key: '438451982634141',
-    api_secret: 'lqhr7oCDXoMKma_owPak_Ls-_F8'
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET
 });
 
 const storage = new CloudinaryStorage({

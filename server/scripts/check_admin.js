e2eb3498b66c70e59bdf41294a9af3db423eaef0
dilsec20 <dilipkumar2832005@gmail.com>
2026-01-16 16:35:08 +0530

    chore: Remove a file.

--- server/scripts/check_admin.js
+++
@@ -1,8 +0,0 @@
-const pool = require('../db');
-async function check() {
-    try {
-        const res = await pool.query("SELECT username, user_email, role FROM users WHERE user_email = 'dilipkumar2832005@gmail.com'");
-        console.log(JSON.stringify(res.rows, null, 2));
-    } catch (e) { console.error(e); } finally { pool.end(); }
-}
-check();
